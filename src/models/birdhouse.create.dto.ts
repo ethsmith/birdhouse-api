d680777c059eb7b61e8430209150924f717ba01f
Ethan Smith <ethan.smith1845@gmail.com>
2023-10-08 14:01:27 -0500

    Enforce a min and max length for the birdhouse name

--- src/models/birdhouse.create.dto.ts
+++ src/models/birdhouse.create.dto.ts
@@ -1,7 +1,10 @@
+import { Length } from 'class-validator';
+
 export class BirdhouseCreateDto {
   longitude: number;
 
   latitude: number;
 
+  @Length(4, 16)
   name: string;
 }
--- src/models/birdhouse.update.dto.ts
+++ src/models/birdhouse.update.dto.ts
@@ -1,7 +1,10 @@
+import { Length } from 'class-validator';
+
 export class BirdhouseUpdateDto {
   longitude: number;
 
   latitude: number;
 
+  @Length(4, 16)
   name: string;
 }
